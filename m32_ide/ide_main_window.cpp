a6bcbde42678d92bffe303393488ec149ae41fd2
Daniel Schulte <daniel@schultes-home.de>
2013-11-09 21:14:43 +0100

    Added dock widgets to the IDE. Added some menu actions

--- m32_ide/ide_main_window.cpp
+++ m32_ide/ide_main_window.cpp
@@ -6,22 +6,28 @@ ide_main_window::ide_main_window(QWidget *parent) : QMainWindow(parent)
     QWidget *central_widget=new QWidget();
     layout_main=new QGridLayout(central_widget);
     setCentralWidget(central_widget);
-    /*
-    QMenu *file_menu=new QMenu("File");
-    menuBar()->addMenu(file_menu);
-    */
 
     cpu=new m32_cpu();
 
-    setup_register_monitor();
-    //setup_memory_monitor();
-    setup_debug_button_panel();
     setup_code_editor();
-    setup_cpu_debug_controls();
+    setup_register_monitor();
+    setup_memory_monitor();
+    setup_cpu_output_log();
+    setup_cpu_info_log();
+    setup_debug_buttons();
+    setup_menu_bar();
 }
 
 ide_main_window::~ide_main_window()
 {
+}
+
+void ide_main_window::setup_code_editor()
+{
+    txt_code=new QTextEdit();
+    layout_main->addWidget(txt_code,0,0,2,1);
+    layout_main->setRowStretch(1,1);
+    layout_main->setColumnStretch(0,1);
 }
 
 QLineEdit* ide_main_window::setup_register_monitor_line_edit()
@@ -103,8 +109,7 @@ void ide_main_window::setup_register_monitor()
 
     layout_register_monitor->setVerticalSpacing(1);
 
-    layout_main->addWidget(grp_registers,0,0);
-    layout_main->setColumnStretch(1,1);
+    layout_main->addWidget(grp_registers,0,1);
 
     cpu->callback_after_exec=[&]{
         le_r0->setText(QString::number(cpu->registers[r0],16).toUpper().rightJustified(8,'0'));
@@ -130,26 +135,44 @@ void ide_main_window::setup_register_monitor()
     };
 }
 
-void ide_main_window::setup_code_editor()
-{
-    txt_code=new QTextEdit();
-    layout_main->addWidget(txt_code,1,0,1,2);
-    cpu->callback_print=[&](std::string msg){
-        txt_code->append(QString::fromStdString(msg));
-    };
-}
-
 void ide_main_window::setup_memory_monitor()
 {
     QLabel *lbl_dummy=new QLabel("memory stuff here...");
-    layout_main->addWidget(lbl_dummy,0,2,2,1);
+    layout_main->addWidget(lbl_dummy,1,1);
 }
 
-void ide_main_window::setup_debug_button_panel()
+void ide_main_window::setup_cpu_output_log()
 {
-    QGroupBox *grp_debug_buttons=new QGroupBox("Debug Controls");
+    txt_cpu_log=new QTextEdit();
+    txt_cpu_log->setReadOnly(true);
+    dw_cpu_output=new QDockWidget("CPU Output",this);
+    dw_cpu_output->setAllowedAreas(Qt::BottomDockWidgetArea|Qt::RightDockWidgetArea);
+    dw_cpu_output->setWidget(txt_cpu_log);
+    addDockWidget(Qt::BottomDockWidgetArea,dw_cpu_output);
+    cpu->callback_print=[&](std::string msg){
+        txt_cpu_log->append(QString::fromStdString(msg));
+    };
+}
+
+void ide_main_window::setup_cpu_info_log()
+{
+    txt_cpu_info=new QTextEdit();
+    txt_cpu_info->setReadOnly(true);
+    dw_cpu_info=new QDockWidget("CPU Information",this);
+    dw_cpu_info->setAllowedAreas(Qt::BottomDockWidgetArea|Qt::RightDockWidgetArea);
+    dw_cpu_info->setWidget(txt_cpu_info);
+    addDockWidget(Qt::BottomDockWidgetArea,dw_cpu_info);
+    cpu->callback_log=[&](std::string msg){
+        txt_cpu_info->append(QString::fromStdString(msg));
+    };
+}
+
+void ide_main_window::setup_debug_buttons()
+{
+
+    QWidget *w_debug_buttons=new QWidget();
     QVBoxLayout *layout_debug=new QVBoxLayout();
-    grp_debug_buttons->setLayout(layout_debug);
+    w_debug_buttons->setLayout(layout_debug);
     pb_dbg_load_binary=new QPushButton("Load Binary File");
     pb_dbg_reset=new QPushButton("Reset CPU");
     pb_dbg_step=new QPushButton("Step");
@@ -163,11 +186,11 @@ void ide_main_window::setup_debug_button_panel()
     layout_debug->addWidget(pb_dbg_break);
     layout_debug->addStretch(1);
 
-    layout_main->addWidget(grp_debug_buttons,0,2,2,1);
-}
+    dw_debug_controls=new QDockWidget("Debug Controls",this);
+    dw_debug_controls->setAllowedAreas(Qt::RightDockWidgetArea|Qt::BottomDockWidgetArea);
+    dw_debug_controls->setWidget(w_debug_buttons);
+    addDockWidget(Qt::BottomDockWidgetArea,dw_debug_controls);
 
-void ide_main_window::setup_cpu_debug_controls()
-{
     connect(pb_dbg_load_binary,&QPushButton::clicked,[&]{
         QString file_name=QFileDialog::getOpenFileName(this,"Select binary file to load","","Binaries (*.bin)");
         if(file_name=="")
@@ -186,4 +209,18 @@ void ide_main_window::setup_cpu_debug_controls()
     connect(pb_dbg_run,&QPushButton::clicked,[&]{
         //cpu->run(); //this will hang whithout multithreading
     });
+}
+
+void ide_main_window::setup_menu_bar()
+{
+    QMenu *menu_file=new QMenu("File");
+    QMenu *menu_view=new QMenu("View");
+    QMenu *menu_help=new QMenu("Help");
+    menuBar()->addMenu(menu_file);
+    menuBar()->addMenu(menu_view);
+    menuBar()->addMenu(menu_help);
+
+    menu_view->addAction(dw_debug_controls->toggleViewAction());
+    menu_view->addAction(dw_cpu_output->toggleViewAction());
+    menu_view->addAction(dw_cpu_info->toggleViewAction());
 }
--- m32_ide/ide_main_window.h
+++ m32_ide/ide_main_window.h
@@ -8,6 +8,8 @@
 #include <QPushButton>
 #include <QFileDialog>
 #include <QTextEdit>
+#include <QDockWidget>
+#include <QPlainTextEdit>
 
 #include "m32_cpu.h"
 
@@ -19,17 +21,24 @@ public:
     explicit ide_main_window(QWidget *parent = 0);
     ~ide_main_window();
 private:
+    void setup_code_editor();
+
     QLineEdit* setup_register_monitor_line_edit();
     void setup_register_monitor();
-    void setup_code_editor();
     void setup_memory_monitor();
-    void setup_debug_button_panel();
-    void setup_cpu_debug_controls();
+
+    void setup_cpu_output_log();
+    void setup_cpu_info_log();
+
+    void setup_debug_buttons();
+    void setup_menu_bar();
+
     QGridLayout *layout_main;
     QLineEdit *le_r0,*le_r1,*le_r2,*le_r3,*le_r4,*le_r5,*le_r6,*le_r7,*le_status,*le_pc;
     QLineEdit *le_ssp,*le_usp,*le_sbase,*le_ubase,*le_slimit,*le_ulimit,*le_intflags,*le_intbase,*le_timer,*le_treload;
     QPushButton *pb_dbg_load_binary,*pb_dbg_reset,*pb_dbg_step,*pb_dbg_run,*pb_dbg_break;
-    QTextEdit *txt_code;
+    QTextEdit *txt_code,*txt_cpu_log,*txt_cpu_info;
+    QDockWidget *dw_cpu_output,*dw_cpu_info,*dw_debug_controls;
 
     m32_cpu *cpu;
 };
